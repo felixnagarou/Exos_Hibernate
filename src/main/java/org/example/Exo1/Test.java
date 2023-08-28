48b4d6ce5a078e8bfd3cda54fe786f5e30522d18
HippolyteÃ <hippolyteaubin@gmail.com>
2023-08-28 15:21:42 +0200

    Exo 1 Hibernate

--- src/main/java/org/example/Exo1/Test.java
+++ src/main/java/org/example/Exo1/Test.java
@@ -8,6 +8,8 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.boot.MetadataSources;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,29 +24,31 @@ public class Test {
 
         List<Product> productList = new ArrayList<>();
 
-        for (Product product : productList
-             ) { product.setPrice(10);
+        for (int i = 0; i < 5; i++) {
+            Product product = new Product();
+            product.setPrice(5);
             product.setBrand("Carrefour");
             product.setStock(10);
-            product.setReference("Test");
-            product.setPurchaseDate();
+            product.setReference("Steak");
+            String date = "10/08/2023";
+            LocalDate pDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            product.setPurchaseDate(pDate);
+            productList.add(product);
             session.save(product);
             System.out.println(product.getId());
         }
         session.getTransaction().commit();
 
-        session.load(Product.class, 2L);
 
+        session.getTransaction().begin();
+        Product product = session.load(Product.class, 2L);
+        session.update(product);
+
+        session.getTransaction().begin();
         session.delete(productList.contains(3L));
-
-
-
+        session.getTransaction().commit();
 
         session.close();
         sessionFactory.close();
-
-
-
-
     }
 }
